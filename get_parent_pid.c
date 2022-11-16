be42a759ba264bd1ba63466be85c3a54763a12e1
mhaghili <79529016+mhaghili@users.noreply.github.com>
2022-11-16 12:40:40 +0330

    Add some files

---
+++ get_parent_pid.c
@@ -0,0 +1,66 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+
+  int saved_ebx;
+
+  int pid0, pid1, pid2;
+  printf(1, "test program: forking childs\n\n");
+  pid0 = fork();
+  if (pid0) {
+    asm volatile(
+    "movl %%ebx, %0;"
+    "movl %1, %%ebx;"
+    : "=r" (saved_ebx)
+    : "r"(pid0)
+    );
+    printf(1, "User: printing ancestors for pid: %d\n" , pid0);
+    int parent_pid = get_parent_pid();
+
+    printf(1, "pid: %d, parent_pid: %d\n\n", pid0, parent_pid);
+    asm("movl %0, %%ebx" : : "r"(saved_ebx));
+  }
+  while(wait() != -1);
+    
+  pid1 = fork();
+  if (pid1 && pid0 == 0) {
+    printf(1, "User: printing ancestors for pid: %d\n" , pid1);
+
+    asm volatile(
+    "movl %%ebx, %0;"
+    "movl %1, %%ebx;"
+    : "=r" (saved_ebx)
+    : "r"(pid1)
+    );
+    int parent_pid = get_parent_pid();
+
+    printf(1, "pid: %d, parent_pid: %d\n\n", pid1, parent_pid);
+    asm("movl %0, %%ebx" : : "r"(saved_ebx));
+  }
+  while(wait() != -1);
+
+  pid2 = fork();
+
+  if(pid0 == 0 && pid1 == 0 && pid2){
+    printf(1, "User: printing ancestors for pid: %d\n" , pid2);
+
+    asm volatile(
+    "movl %%ebx, %0;"
+    "movl %1, %%ebx;"
+    : "=r" (saved_ebx)
+    : "r"(pid2)
+  );
+
+  int parent_pid = get_parent_pid();
+
+    printf(1, "pid: %d, parent_pid: %d\n\n", pid2, parent_pid);
+    asm("movl %0, %%ebx" : : "r"(saved_ebx));
+  }  
+  while(wait() != -1);
+  
+    
+  exit();
+}
---
+++ largest_prime_factor.c
@@ -0,0 +1,30 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[]){
+	if(argc < 2){
+		printf(2, "Enter one number asshole!\n");
+		exit();
+	}
+
+    else
+    {
+		int saved_ebx, number;
+        number = atoi(argv[1]);
+
+		asm volatile(
+			"movl %%ebx, %0;" // saved_ebx = ebx
+			"movl %1, %%ebx;" // ebx = number
+			: "=r" (saved_ebx)
+			: "r"(number)
+		);
+
+		printf(1, "User: find_largest_prime_factor() called for number: %d\n" , number);
+		printf(1, "Largest prime factor of %d is: %d\n" , number , find_largest_prime_factor());
+		asm("movl %0, %%ebx" : : "r"(saved_ebx)); // ebx = saved_ebx -> restore
+		exit();  	
+    }
+
+    exit();
+} 
